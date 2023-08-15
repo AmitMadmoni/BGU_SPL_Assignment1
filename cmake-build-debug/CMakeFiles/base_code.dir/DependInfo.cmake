d31c179bf6c0034b0a6a6b42f5258865c48dadb9
AmitMadmoni <amamitmadmoni@gmail.com>
2023-08-15 18:42:21 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/base_code.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/spl211/Project 1/209502376-318568383/src/Action.cpp" "CMakeFiles/base_code.dir/src/Action.cpp.o" "gcc" "CMakeFiles/base_code.dir/src/Action.cpp.o.d"
+  "/home/spl211/Project 1/209502376-318568383/src/Customer.cpp" "CMakeFiles/base_code.dir/src/Customer.cpp.o" "gcc" "CMakeFiles/base_code.dir/src/Customer.cpp.o.d"
+  "/home/spl211/Project 1/209502376-318568383/src/Studio.cpp" "CMakeFiles/base_code.dir/src/Studio.cpp.o" "gcc" "CMakeFiles/base_code.dir/src/Studio.cpp.o.d"
+  "/home/spl211/Project 1/209502376-318568383/src/Trainer.cpp" "CMakeFiles/base_code.dir/src/Trainer.cpp.o" "gcc" "CMakeFiles/base_code.dir/src/Trainer.cpp.o.d"
+  "/home/spl211/Project 1/209502376-318568383/src/Workout.cpp" "CMakeFiles/base_code.dir/src/Workout.cpp.o" "gcc" "CMakeFiles/base_code.dir/src/Workout.cpp.o.d"
+  "/home/spl211/Project 1/209502376-318568383/src/main.cpp" "CMakeFiles/base_code.dir/src/main.cpp.o" "gcc" "CMakeFiles/base_code.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/base_code.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/base_code.dir/src/Action.cpp.o"
+  "CMakeFiles/base_code.dir/src/Action.cpp.o.d"
+  "CMakeFiles/base_code.dir/src/Customer.cpp.o"
+  "CMakeFiles/base_code.dir/src/Customer.cpp.o.d"
+  "CMakeFiles/base_code.dir/src/Studio.cpp.o"
+  "CMakeFiles/base_code.dir/src/Studio.cpp.o.d"
+  "CMakeFiles/base_code.dir/src/Trainer.cpp.o"
+  "CMakeFiles/base_code.dir/src/Trainer.cpp.o.d"
+  "CMakeFiles/base_code.dir/src/Workout.cpp.o"
+  "CMakeFiles/base_code.dir/src/Workout.cpp.o.d"
+  "CMakeFiles/base_code.dir/src/main.cpp.o"
+  "CMakeFiles/base_code.dir/src/main.cpp.o.d"
+  "base_code"
+  "base_code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
